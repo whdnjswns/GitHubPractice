c143a7995158e37fb69e82fa2063de741cde718c
dwcho <dwcho@yahoo.com>
2020-08-06 22:57:59 -0400

    add 2 override, view.....

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func viewLayoutMarginsDidChange() {
+        //
+    }
+    override func viewDidLayoutSubviews() {
+        //
+    }
+    
 }
+
 
