962e14d7c05c5c0082cfec85e890f51b7e2195af
dwcho <dwcho@yahoo.com>
2020-08-06 22:49:26 -0400

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Admin on 8/6/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
